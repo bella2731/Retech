54275cac5762a2e426e31a14a353b6554af40676
bella2731 <biela.diplomate@gmail.com>
2025-07-15 15:24:58 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";      
-$password = "";          
+$username = "qwerty";      
+$password = "12345";          
 $dbname = "report"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
