ef78bfe0009295fd8ae7903b371b16a901c01ef1
bella2731 <biela.diplomate@gmail.com>
2025-07-15 18:17:04 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "";      
+$username = "root";      
 $password = "";          
-$dbname = "retech"; 
+$dbname = "report"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
